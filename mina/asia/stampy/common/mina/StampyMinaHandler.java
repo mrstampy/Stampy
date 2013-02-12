54fd9249ecf0752ef6f863685a948ee0f3e7f7d7
burton <burt.alexander@gmail.com>
2013-02-13 00:07:47 +1000

    removed test code

--- mina/asia/stampy/common/mina/StampyMinaHandler.java
+++ mina/asia/stampy/common/mina/StampyMinaHandler.java
@@ -298,16 +298,11 @@ public abstract class StampyMinaHandler extends IoHandlerAdapter {
    *           the exception
    */
   protected void notifyListeners(StampyMessage<?> sm, IoSession session, HostPort hostPort) throws Exception {
-    int idx = 0;
     for (StampyMinaMessageListener listener : listeners) {
-      if(idx == 0 && !(listener instanceof SecurityMinaMessageListener)) {
-        System.out.println("BOOO!!!!");
-      }
       if (isForType(listener.getMessageTypes(), sm.getMessageType()) && listener.isForMessage(sm)) {
         log.trace("Evaluating message {} with listener {}", sm, listener);
         listener.messageReceived(sm, session, hostPort);
       }
-      idx++;
     }
   }
 
